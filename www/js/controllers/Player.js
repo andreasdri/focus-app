b43fe1163db0da672fa7ebe6c7b72265cc71b810
tronghn <pzzo88@gmail.com>
2015-09-10 20:05:35 +0200

    Move audio player logic to AudioLibrary

--- www/js/controllers/Player.js
+++ www/js/controllers/Player.js
@@ -1,8 +1,8 @@
 angular.module('focus.controllers')
-  .controller('PlayerController', function($scope, AudioLibrary, $cordovaMedia2) {
+  .controller('PlayerController', function($scope, AudioLibrary) {
   	var media;
 
-    $scope.isPlaying = false;
+    $scope.isPlaying = AudioLibrary.isPlaying;
     $scope.sounds = AudioLibrary.getAllSounds();
     $scope.sound = AudioLibrary.getSound();
     $scope.currentTrack = $scope.sound.trackNumber - 1;
@@ -11,67 +11,25 @@ angular.module('focus.controllers')
     $scope.$watch(function() { return AudioLibrary.sound }, function(newVal, oldVal) {
     if (typeof newVal !== 'undefined') {
         $scope.sound = AudioLibrary.sound;
-        $scope.new($scope.sound);
       }
-    });
+    })
 
-    $scope.new = function(sound) {
-      if (media) {
-        media.release();
+    $scope.$watch(function() { return AudioLibrary.isPlaying }, function(newVal, oldVal) {
+    if (typeof newVal !== 'undefined') {
+        $scope.isPlaying = AudioLibrary.isPlaying;
       }
-      var src = (ionic.Platform.isAndroid() ? "/android_asset/www/" + sound.src : sound.src);
-
-      media = $cordovaMedia2.newMedia(src);
-
-      $scope.isPlaying = true;
-      $scope.currentTrack = sound.trackNumber - 1;
-
-      media.play().then(function() {
-        // success
-        console.log('finished playback');
-        }, null, function(data) {
-          console.log('track progress: ' + data.position);
-
-          if (data.status) {
-            console.log('track status change: ' + data.status);
-          }
-          if (data.duration) {
-            console.log('track duration: ' + data.duration);
-         }
-      });
-    }
+    })
 
     $scope.playOrPause = function() {
-      if(media && $scope.isPlaying) {
-        $scope.isPlaying = false;
-        media.pause();
-      }
-      else if (media && !$scope.isPlaying) {
-        $scope.isPlaying = true;
-        media.play();
-      }
+      AudioLibrary.playOrPause();
     }
 
     $scope.next = function() {
-      if ($scope.currentTrack == $scope.sounds.length - 1) {
-        $scope.sound = $scope.sounds[0];
-        $scope.new($scope.sounds[0]);
-      }
-      else {
-        $scope.sound = $scope.sounds[$scope.currentTrack + 1];
-        $scope.new($scope.sounds[$scope.currentTrack + 1]);
-       
-      }
+      AudioLibrary.next();
     }
 
     $scope.prev = function() {
-      if ($scope.currentTrack == 0) {
-        $scope.sound = $scope.sounds[$scope.sounds.length - 1];
-        $scope.new($scope.sounds[$scope.sounds.length - 1]);
-      }
-      else {
-        $scope.sound = $scope.sounds[$scope.currentTrack - 1];
-        $scope.new($scope.sounds[$scope.currentTrack - 1]);
-      }
+      AudioLibrary.prev();
     }
+
   });
