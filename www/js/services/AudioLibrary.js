a566b5e77e1bf70efd60b55b388173b069714654
erlendbu <erlendbu@stud.ntnu.no>
2015-09-15 11:22:41 +0200

    Correct description text has been added to the audiolibrary

--- www/js/services/AudioLibrary.js
+++ www/js/services/AudioLibrary.js
@@ -5,21 +5,24 @@ angular.module('focus.services')
         src: 'sounds/basic/01.mp3',
         title: 'Kunsten å slappe av!',
         trackNumber: 1,
-        description: 'En kortversjon av progressiv...',
+        description: 'En kortversjon av progressiv avspenning blir her introdusert (ca 12 minutter). Programmet å slappe av gjennom kontrastøvelser, samt at det mot slutten tar for seg kroppsscanning.',
         art: 'img/ionic.png'
       },
       {
         src: 'sounds/basic/02.mp3',
         title: 'Om å sette seg mål',
         trackNumber: 2,
-        description: 'En kortversjon av progressiv...',
+        description: 'Dette programmet kombinerer bruk av visualisering og det å sette seg konkrete mål for hver treningsøkt. Programmet tar også for seg hvordan du kan utfordre negative tanker og snu dette til en mer konstruktiv tenkning.',
         art: 'img/ionic.png'
       },
       {
         src: 'sounds/basic/03.mp3',
         title: 'Den jeg er / Meg',
         trackNumber: 3,
-        description: 'En kortversjon av progressiv...',
+        description: 'Programmet retter seg mot det å ha et selvbilde som bygger på egne premisser uavhengig av hva alle \
+                      andre måtte mene og uavhengig av prestasjon. Innholdet tar for seg hvordan du bør se på deg selv \
+                      utfra egne kriterier og hva som er viktig for deg. Hensikten er å løsrive seg fra hva alle andre måtte si \
+                      og mene og skape større trygghet for egne vurderinger og meninger.',
         art: 'img/ionic.png'
       },
       {
@@ -27,7 +30,9 @@ angular.module('focus.services')
         title: 'Mental tøffhet',
         subtitle: '- kunsten å takle forstyrrelser!',
         trackNumber: 4,
-        description: 'En kortversjon av progressiv...',
+        description: 'Her er fokus å trene på det å takle ulike situasjoner eller påvirkninger som kan oppstå i løpet av en \
+                      konkurranse, men også i livet generelt. Det dreier seg nødvendigvis ikke om å ta bort stresset men å \
+                      lære seg å håndtere det på en bedre måte',
         art: 'img/ionic.png'
       },
       {
@@ -35,49 +40,51 @@ angular.module('focus.services')
         title: 'Time-out',
         subtitle: '- en pust i hverdagen, oppmerksomhetstrening',
         trackNumber: 5,
-        description: 'En kortversjon av progressiv...',
+        description: 'Oppmerksomhetstrening er en viktig ingrediens i den mentale treningen på elitenivå. Her får utøveren \
+                      en liten forsmak på hvordan denne type trening kan være.',
         art: 'img/ionic.png'
       },
       {
         src: 'sounds/basic/06.mp3',
         title: 'Instrumental',
         trackNumber: 6,
-        description: 'En kortversjon av progressiv...',
+        description: 'TODO: Find something to put here',
         art: 'img/ionic.png'
       },
       {
         src: 'sounds/basic/07.mp3',
         title: 'Konkurranseforberedelser',
         trackNumber: 7,
-        description: 'En kortversjon av progressiv...',
+        description: 'Hvordan det hele henger sammen. Hvordan måten du tenker på påvirker inngangen til konkurransen \
+                      eller testen og hvordan du best mulig forbereder deg for å få til en optimal prestasjon.',
         art: 'img/ionic.png'
       },
       {
         src: 'sounds/basic/08.mp3',
         title: 'Nullstille',
         trackNumber: 8,
-        description: 'En kortversjon av progressiv...',
+        description: 'Refokus etter en konkurranse eller test – enten det har gått bra eller dårlig!',
         art: 'img/ionic.png'
       },
       {
         src: 'sounds/basic/09.mp3',
         title: 'Visualisering',
         trackNumber: 9,
-        description: 'En kortversjon av progressiv...',
+        description: 'TODO: Find something to put here',
         art: 'img/ionic.png'
       },
       {
         src: 'sounds/basic/10.mp3',
         title: 'Søvn',
         trackNumber: 10,
-        description: 'En kortversjon av progressiv...',
+        description: 'Variant av kroppscanning dersom en sliter med søvn.',
         art: 'img/ionic.png'
       },
       {
         src: 'sounds/basic/11.mp3',
         title: 'Instrumental - post søvn',
         trackNumber: 11,
-        description: 'En kortversjon av progressiv...',
+        description: 'TODO: Find something to put here',
         art: 'img/ionic.png'
       }
     ];
@@ -90,6 +97,6 @@ angular.module('focus.services')
         return sounds;
       }
 
-    };  
-    
+    };
+
   });
