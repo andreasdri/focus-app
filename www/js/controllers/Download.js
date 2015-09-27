6719a7de58350d5084be58617e69e2356892b68a
tronghn <pzzo88@gmail.com>
2015-09-27 13:03:31 +0200

    Add support for downloading files from server

--- www/js/controllers/Download.js
+++ www/js/controllers/Download.js
@@ -4,20 +4,34 @@ angular.module('focus.controllers')
   $scope.downloadFile = function() {
 
     var directory = cordova.file.applicationStorageDirectory;
+    var username = "test";
+    var password = "test";
+    var url = encodeURI("http://localhost:3000/download/");
+    var targetPath = directory + "basic.zip";
+    var trustHosts = true;
+    var authdata = btoa(username + ":" + password);
+    var options = {
+      headers: {
+        "Authorization": "Basic " + authdata
+      }
+    };
 
-    $scope.images = [];
+    $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+      .then(function(result) {
+        // Success!
+        console.log("Files downloaded " + JSON.stringify(result.response));
+      }, function(err) {
+        // Error
+        console.log("Files failed to download " + JSON.stringify(err));
+      }, function (progress) {
+        $timeout(function () {
+          $scope.downloadProgress = (progress.loaded / progress.total) * 100;
+        })
+      });
 
-    var resources = [
-        "http://imaxfocus.com/heleneO_150x150.jpg",
-        "http://imaxfocus.com/olaf_tufte.jpg",
-        "http://imaxfocus.com/alexander_dale_oen.jpg",
-        "http://imaxfocus.com/mette_solli.jpg",
-        "http://imaxfocus.com/tutta1.jpg"
-      ];
+   };
 
-    var promises = [];
-
-    resources.forEach(function(i,x) {
+    /*resources.forEach(function(i,x) {
       var fileName = i.split("/").pop();
       var targetPath = directory + fileName;
       $cordovaFile.checkFile(directory, fileName)
@@ -33,21 +47,26 @@ angular.module('focus.controllers')
     // Download asset if it doesn't already exist
     function downloadAsset(url, targetPath) {
       console.log("About to start transfer");
-      promises.push($cordovaFileTransfer.download(url, targetPath, {}, true));
+      var authdata = btoa(username + ':' + password);
+      console.log(authdata);
+      promises.push($cordovaFileTransfer.download(url, targetPath,
+        {
+          "Authorization": "Basic " + authdata
+        }, true));
     }
 
     // Refer to asset on device if it already exists
     function referToAsset(targetPath) {
-      $scope.images.push(targetPath);
+      $scope.files.push(targetPath);
     }
 
     $q.all(promises).then(function(res) {
       console.log("in theory, all done");
       for(var i=0; i<res.length; i++) {
-        $scope.images.push(res[i].nativeURL);
+        $scope.files.push(res[i]);
       }
     });
 
-   };
+   };*/
 
 });
