964d93b5cf49b889d31efc27fbbb52889f29c8e2
tronghn <pzzo88@gmail.com>
2015-09-28 11:24:49 +0200

    Enable unzipping of files on device

--- www/js/controllers/Download.js
+++ www/js/controllers/Download.js
@@ -1,5 +1,5 @@
 angular.module('focus.controllers')
-	.controller('DownloadController', function($scope, $timeout, $cordovaFileTransfer, $cordovaFile, $q) {
+	.controller('DownloadController', function($scope, $timeout, $cordovaFileTransfer, $cordovaFile, $cordovaZip, $q) {
 
   $scope.downloadFile = function() {
 
@@ -7,19 +7,31 @@ angular.module('focus.controllers')
     var username = "test";
     var password = "test";
     var url = encodeURI("http://localhost:3000/download/");
-    var targetPath = directory + "basic.zip";
+    var fileName = "basic.zip"
+    var targetPath = directory + fileName;
     var trustHosts = true;
-    var authdata = btoa(username + ":" + password);
+    var authData = btoa(username + ":" + password);
     var options = {
       headers: {
-        "Authorization": "Basic " + authdata
+        "Authorization": "Basic " + authData
       }
     };
 
-    $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+    $cordovaFile.checkFile(directory, fileName)
+    .then(function (success) {
+        console.log("Asset found on device, skipping download, unzipping");
+        unzip();
+      }, function (error) {
+        console.log("Asset not found on device, initiating download");
+        download();
+      });
+
+    function download() {
+      $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
       .then(function(result) {
         // Success!
         console.log("Files downloaded " + JSON.stringify(result.response));
+        unzip();
       }, function(err) {
         // Error
         console.log("Files failed to download " + JSON.stringify(err));
@@ -28,54 +40,20 @@ angular.module('focus.controllers')
           $scope.downloadProgress = (progress.loaded / progress.total) * 100;
         })
       });
+    };
 
-   };
-
-   /*
-    $scope.images = [];
-
-    var resources = [
-        "http://imaxfocus.com/heleneO_150x150.jpg",
-        "http://imaxfocus.com/olaf_tufte.jpg",
-        "http://imaxfocus.com/alexander_dale_oen.jpg",
-        "http://imaxfocus.com/mette_solli.jpg",
-        "http://imaxfocus.com/tutta1.jpg"
-      ];
-
-    var promises = [];
-
-    resources.forEach(function(i,x) {
-      var fileName = i.split("/").pop();
-      var targetPath = directory + fileName;
-      $cordovaFile.checkFile(directory, fileName)
-      .then(function (success) {
-        console.log("Asset found on device, skipping download");
-        referToAsset(targetPath);
-      }, function (error) {
-        console.log("Asset not found on device, initiating download");
-        downloadAsset(i, targetPath);
+    function unzip() {
+      $cordovaZip.unzip(directory + fileName,
+        directory + "sounds")
+      .then(function () {
+        console.log('Files unzipped');
+      }, function () {
+        console.log('Failed to unzip');
+      }, function (progressEvent) {
+        console.log(progressEvent);
       });
-    });
+    };
 
-    // Download asset if it doesn't already exist
-    function downloadAsset(url, targetPath) {
-      console.log("About to start transfer");
-      promises.push($cordovaFileTransfer.download(url, targetPath, {}, true));
-    }
-
-    // Refer to asset on device if it already exists
-    function referToAsset(targetPath) {
-      $scope.images.push(targetPath);
-    }
-
-    $q.all(promises).then(function(res) {
-      console.log("in theory, all done");
-      for(var i=0; i<res.length; i++) {
-        $scope.images.push(res[i]);
-      }
-    });
-
-   };
-   */
+  };
 
 });
