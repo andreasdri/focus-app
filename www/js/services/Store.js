9d64448ebd829bac813e4c3ea6a622bad2eec421
erlendbu <erlendbu@stud.ntnu.no>
2015-10-22 13:59:12 +0200

    Updated play-market apk and added basic access to the cordova-purchase-plugin

---
+++ www/js/services/Store.js
@@ -0,0 +1,43 @@
+angular.module('focus.services')
+  .factory('Store', function() {
+
+
+
+    var functions = {
+      initializeStore: function() {
+
+        // Let's set a pretty high verbosity level, so that we see a lot of stuff
+        // in the console (reassuring us that something is happening).
+        store.verbosity = store.INFO;
+
+        // We register a dummy product. It's ok, it shouldn't
+        // prevent the store "ready" event from firing.
+        store.register({
+          id:    "com.example.app.inappid1",
+          alias: "100 coins",
+          type:  store.CONSUMABLE
+        });
+
+        // When every goes as expected, it's time to celebrate!
+        // The "ready" event should be welcomed with music and fireworks,
+        // go ask your boss about it! (just in case)
+        store.ready(function() {
+          console.log("\\o/ STORE READY \\o/");
+        });
+
+        // After we've done our setup, we tell the store to do
+        // it's first refresh. Nothing will happen if we do not call store.refresh()
+        store.refresh();
+
+        store.ready(function() {
+          console.log("\\o/ STORE READY \\o/");
+        });
+
+        console.log(store.get("100 coins").alias)
+
+      }
+    };
+
+    return functions;
+
+})
