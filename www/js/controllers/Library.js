fc3af31a2ed4670503a9c69317d7eca59eaa17c7
Andreas Drivenes <andreas.drivenes@gmail.com>
2015-10-22 12:54:54 +0200

    Add training program from library
    Automatically go to the second slide of adding
    program from the library.

--- www/js/controllers/Library.js
+++ www/js/controllers/Library.js
@@ -1,5 +1,7 @@
 angular.module('focus.controllers')
-	.controller('LibraryController', function($scope, AudioLibrary, AudioPlayer, $state) {
+	.controller('LibraryController', function($scope, AudioLibrary, AudioPlayer,
+     $rootScope, $state) {
+
     $scope.sounds = AudioLibrary.getAllSounds();
   	$scope.setSound = function(trackNumber) {
   		AudioPlayer.setSound(trackNumber);
@@ -10,5 +12,13 @@ angular.module('focus.controllers')
     $scope.viewDetails = function(sound) {
       $state.go('main.library.chapterinfo',{'selectedSound': sound });
     };
-
+    $scope.selectedSound = function(sound) {
+      $rootScope.select = sound;
+    }
+    $scope.addProgram = function(evt, sound) {
+      evt.stopPropagation();
+      $state.go('main.mytraining.addprogram',
+        {'selectedSound': sound, 'selectedCategory': sound.category}
+      );
+    }
   });
