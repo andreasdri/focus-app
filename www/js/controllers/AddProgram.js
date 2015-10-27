6547aa6676a0c247e3e22e6e236ebf1d7b79c3ea
tronghn <pzzo88@gmail.com>
2015-10-27 19:29:51 +0100

    Update 'Add Program' view
    Update the 'Add Program' view to reduce verticality, including changing
    up the checkboxes, and moving longer textual information to popups (for
    now, maybe change to subviews?). Add a 'next' button in the 'Choose
    Category' slide and a 'back' button in the 'Choose Program' slide (back to
    'Choose Category'). Make program buttons in 'Choose Program' toggleable, defaults to none selected.

--- www/js/controllers/AddProgram.js
+++ www/js/controllers/AddProgram.js
@@ -1,7 +1,7 @@
 angular.module('focus.controllers')
   .controller('AddProgramController', function($scope, $ionicSlideBoxDelegate,
       SoundCategory, AudioLibrary, $stateParams, soundsByCategoryFilter,
-      $timeout, TrainingProgram, $state) {
+      $timeout, TrainingProgram, $state, $ionicPopup, $ionicHistory) {
 
     var getCategory = function(cat) {
       for(var i = 0; i < $scope.categories.length; i++) {
@@ -13,23 +13,44 @@ angular.module('focus.controllers')
         }
       }
       return null;
-    }
+    };
 
     var allSounds = AudioLibrary.getAllSounds();
     $scope.categories = SoundCategory.getCategories();
-    $scope.selectedSound = $stateParams.selectedSound || allSounds[0];
+    $scope.selectedSound = $stateParams.selectedSound;
     $scope.selectedCategory = getCategory($stateParams.selectedCategory) || $scope.categories[0];
 
     $scope.sounds = soundsByCategoryFilter(allSounds, $scope.selectedCategory);
+    $scope.slideIndex = 0;
 
     $scope.toggleActiveCategory = function(category) {
       $scope.selectedCategory = category;
       $scope.sounds = soundsByCategoryFilter(allSounds, $scope.selectedCategory);
-      $scope.selectedSound = $scope.sounds[0];
     };
 
     $scope.toggleActiveSound = function(sound) {
-      $scope.selectedSound = sound;
+      if ($scope.selectedSound !== null && sound === $scope.selectedSound) {
+        $scope.selectedSound = null;
+      } else {
+        $scope.selectedSound = sound;
+      }
+    };
+
+    $scope.nextSlide = function() {
+      $ionicSlideBoxDelegate.$getByHandle('program').next();
+    };
+
+    $scope.goBack = function() {
+      if ($scope.slideIndex === 0) {
+        $ionicHistory.goBack();
+      }
+      else {
+        $ionicSlideBoxDelegate.$getByHandle('program').previous();
+      }
+    };
+
+    $scope.slideChanged = function(index) {
+      $scope.slideIndex = index;
     };
 
     $scope.addProgram = function(program) {
@@ -45,8 +66,35 @@ angular.module('focus.controllers')
         $state.go('main.mytraining');
       });
 
-    }
+    };
 
+    $scope.showCategoryInfo = function() {
+      var categoryPopup = $ionicPopup.show({
+        template: '<style>.popup-title { font-size:16px; }</style><p class="padding">{{selectedCategory.description}}</p>',
+        title: $scope.selectedCategory.title,
+        scope: $scope,
+        buttons: [
+          {
+            text: '<b>OK</b>',
+            type: 'button-royal',
+          }
+        ]
+      });
+     };
+
+    $scope.showProgramInfo = function() {
+      var programPopup = $ionicPopup.show({
+        template: '<style>.popup-title { font-size:16px; }</style><p class="padding">{{sound.description}}</p>',
+        title: $scope.selectedSound.title,
+        scope: $scope,
+        buttons: [
+          {
+            text: '<b>OK</b>',
+            type: 'button-royal',
+          }
+        ]
+      });
+     };
   })
   .filter('soundsByCategory', function() {
     return function(sounds, category) {
